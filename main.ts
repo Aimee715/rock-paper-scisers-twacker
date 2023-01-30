9e9fdd8a7eade6e71a73d91e0b9956407228a622
Aimee715 <122555187+Aimee715@users.noreply.github.com>
2023-01-30 12:18:33 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+function UPDATE_SCORE () {
+    OLED.clear()
+    OLED.newLine()
+    OLED.writeStringNewLine("PLAYER A: " + PA)
+    OLED.newLine()
+    OLED.writeStringNewLine("PLAYER B: " + PB)
+    OLED.newLine()
+    OLED.writeStringNewLine("PLAYER T: " + TIES)
+    OLED.newLine()
+    OLED.writeStringNewLine("PLAYER ROUNDS: " + rounds)
+}
+let rounds = 0
+let TIES = 0
+let PB = 0
+let PA = 0
+OLED.init(128, 64)
+PA = 0
+PB = 0
+TIES = 0
+rounds = 0
+OLED.writeStringNewLine("LETS PLAY A GAME DUMMY")
+basic.pause(2000)
+UPDATE_SCORE()
