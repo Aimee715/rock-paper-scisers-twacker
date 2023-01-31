97302e10f980f5c1437e0a5f6f1f4b6992b5287c
Aimee715 <122555187+Aimee715@users.noreply.github.com>
2023-01-31 11:55:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    PA += 1
+    rounds += 1
+    UPDATE_SCORE()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("T")
+    TIES += 1
+    rounds += 1
+    UPDATE_SCORE()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    PB += 1
+    rounds += 1
+    UPDATE_SCORE()
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
 function UPDATE_SCORE () {
     OLED.clear()
     OLED.newLine()
@@ -9,15 +30,18 @@ function UPDATE_SCORE () {
     OLED.newLine()
     OLED.writeStringNewLine("PLAYER ROUNDS: " + rounds)
 }
-let rounds = 0
-let TIES = 0
+function reset () {
+    OLED.init(128, 64)
+    PA = 0
+    PB = 0
+    TIES = 0
+    rounds = 0
+    OLED.writeStringNewLine("lets play the game")
+    basic.pause(2000)
+    UPDATE_SCORE()
+}
 let PB = 0
+let TIES = 0
+let rounds = 0
 let PA = 0
-OLED.init(128, 64)
-PA = 0
-PB = 0
-TIES = 0
-rounds = 0
-OLED.writeStringNewLine("LETS PLAY A GAME DUMMY")
-basic.pause(2000)
-UPDATE_SCORE()
+reset()
